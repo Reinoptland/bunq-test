e1c4268c230712826e67878f7057f558f215722b
Charise Walraven <cherri.walraven@gmail.com>
2018-05-15 11:38:50 +0200

    Begin setup and copy/paste login from game starter

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <nav>
+          </nav>
+          <main>
+            <Route exact path='/signup' component={SignupPage}/>
+            <Route exact path='/login' component={LoginPage}/>
+            <Route exact path="/" render={() => <Redirect to="/transactions" />} />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
