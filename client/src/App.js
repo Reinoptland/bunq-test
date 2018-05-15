e9c84bb80b0a44175e84572c3bb2f4212a270f39
Charise Walraven <cherri.walraven@gmail.com>
2018-05-15 14:01:35 +0200

    add login, logout, signup, service worker and icons for homescreen and splash screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import LoginPage from './components/login/LoginPage'
+// import SignupPage from './components/signup/SignupPage'
+import LogoutPage from './components/logout/LogoutPage'
+// import TransactionsPage from './components/transactions/TransactionsPage'
+// import ContractsPage from './components/contracts/ContractsPage'
+import register, { unregister } from './registerServiceWorker'
 
 class App extends Component {
+  componentWillMount(){
+    register()
+  }
+
+  componentWillUnmount(){
+    unregister()
+  }
   render() {
     return (
       <Router>
@@ -11,8 +23,12 @@ class App extends Component {
           <nav>
           </nav>
           <main>
-            <Route exact path='/signup' component={SignupPage}/>
+            {/* <Route exact path='/signup' component={SignupPage}/> */}
             <Route exact path='/login' component={LoginPage}/>
+            <Route exact path="/logout" component={LogoutPage} />
+            {/* <Route exact path='/bunq' component={BunqPage}/> */}
+            {/* <Route exact path='/transactions' component={TransactionsPage}/> */}
+            {/* <Route exact path='/contracts' component={ContractsPage}/> */}
             <Route exact path="/" render={() => <Redirect to="/transactions" />} />
           </main>
         </div>
---
+++ client/src/reducers/index.js
@@ -0,0 +1,11 @@
+import users from './users'
+import login from './login'
+import signup from './signup'
+import currentUser from './currentUser'
+
+export default {
+  users,
+  login,
+  currentUser,
+  signup
+}
