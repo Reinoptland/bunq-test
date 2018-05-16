6a12a75ab005a8d9f2c6214b34c29bf009c50687
Charise Walraven <cherri.walraven@gmail.com>
2018-05-16 12:24:43 +0200

    add logo and try to import it

--- client/src/components/layout/topBar.jsx
+++ client/src/components/layout/topBar.jsx
@@ -10,45 +10,45 @@ import { connect } from 'react-redux'
 
 
 const TopBar = (props) => {
-    const { location, history, user } = props
+  const { location, history, user } = props
 
-    return (
-        <AppBar position="absolute" style={{ fontFamily:'', background: 'linear-gradient(to right, #E84435, #F57F17)', zIndex: 10 }}>
-            <Toolbar>
-                <Typography variant="title" color="inherit" style={{ textAlign: 'center', flex: 1 }}>
-                        Roos - need to add svg or png
+  return (
+    <AppBar position="absolute" style={{ fontFamily: '', background: 'linear-gradient(to right, #E84435, #F57F17)', zIndex: 10 }}>
+      <Toolbar>
+        <Typography variant="title" color="inherit" style={{ textAlign: 'center', flex: 1 }}>
+          roos
                 </Typography>
-                {
-                    user &&
-                    <Button color="inherit">{user.firstName}</Button>
-                }
+        {
+          user &&
+          <Button color="inherit">{user.firstName}</Button>
+        }
 
-                {
-                    location.pathname.indexOf('signup') > 0 &&
-                    <Button color="inherit" onClick={() => history.push('/login')}>Login</Button>
-                }
-                {
-                    location.pathname.indexOf('login') > 0 &&
-                    <Button color="inherit" onClick={() => history.push('/signup')}>Sign up</Button>
-                }
-                {
-                    location.pathname.indexOf('games/') > 0 &&
-                    <Button color="inherit" onClick={() => history.push('/dashboard')}>Dashboard</Button>
-                }
-                {
-                    /games$/.test(location.pathname) &&
-                    <Button color="inherit" onClick={() => history.push('/logout')}>Log out</Button>
-                }
-            </Toolbar>
-        </AppBar>
-    )
+        {
+          location.pathname.indexOf('signup') > 0 &&
+          <Button color="inherit" onClick={() => history.push('/login')}>Login</Button>
+        }
+        {
+          location.pathname.indexOf('login') > 0 &&
+          <Button color="inherit" onClick={() => history.push('/signup')}>Sign up</Button>
+        }
+        {
+          location.pathname.indexOf('games/') > 0 &&
+          <Button color="inherit" onClick={() => history.push('/dashboard')}>Dashboard</Button>
+        }
+        {
+          /games$/.test(location.pathname) &&
+          <Button color="inherit" onClick={() => history.push('/logout')}>Log out</Button>
+        }
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 const mapStateToProps = state => ({
-    user: state.currentUser && state.users &&
-        state.users[userId(state.currentUser.jwt)]
+  user: state.currentUser && state.users &&
+    state.users[userId(state.currentUser.jwt)]
 })
 
 export default withRouter(
-    connect(mapStateToProps)(TopBar)
+  connect(mapStateToProps)(TopBar)
 )
