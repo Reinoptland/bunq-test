dc68aafbe530d607c7a8567445f322ba031b965c
Preeti <singhp1992@gmail.com>
2018-05-16 14:24:13 +0200

    changing all things named transactions to dashboard - rerouting to dashboard page now

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import LoginPage from './components/login/LoginPage'
 import LogoutPage from './components/logout/LogoutPage'
-import TransactionsPage from './components/dashboard/TransactionsPage'
+import DashboardPage from './components/dashboard/DashboardPage'
 // import ContractsPage from './components/contracts/ContractsPage'
 import register, { unregister } from './registerServiceWorker'
 import SignupPage from './components/signup/SignupPage'
@@ -31,10 +31,10 @@ class App extends Component {
             <Route exact path="/logout" component={LogoutPage} />
             <Route exact path='/bunq' component={BunqPage}/>
             {/* <Route exact path='/feedback' component={FeedbackPage} /> */}
-            <Route exact path='/transactions' component={TransactionsPage}/>
+            <Route exact path='/dashboard' component={DashboardPage}/>
             {/* <Route exact path='/contracts' component={ContractsPage}/> */}
             <Route exact path='/signup' component={SignupPage}/>
-            <Route exact path="/" render={() => <Redirect to="/transactions" />} />
+            <Route exact path="/" render={() => <Redirect to="/dashboard" />} />
           </main>
         </div>
       </Router>
