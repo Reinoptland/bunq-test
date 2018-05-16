13884c2f80d69c7740f7639507a1332098036bda
Charise Walraven <cherri.walraven@gmail.com>
2018-05-16 17:44:14 +0200

    calculate transactions

--- client/src/components/dashboard/DashboardPage.jsx
+++ client/src/components/dashboard/DashboardPage.jsx
@@ -5,6 +5,20 @@ import { connect } from 'react-redux'
 import { fetchTransactions } from '../../actions/transactions'
 import { Redirect } from 'react-router-dom'
 
+
+const calculateTransactions = (arr) => {
+  let insurance = 0 
+  let telecom = 0  
+  let energy = 0 
+arr.map(t => {
+  let value =  (- Number(t.value))
+  if(t.type === 'insurance') insurance = insurance + value
+  if(t.type === 'telecom') telecom = telecom + value
+  if(t.type === 'energy') energy = energy + value
+})
+  return [["Category", "Amount"], ["Insurance", insurance], ["Telecom", telecom], ["Energy", energy]]
+}
+
 class DashboardPage extends PureComponent {
   componentWillMount() {
     if (this.props.transactions === null) {
@@ -15,18 +29,24 @@ class DashboardPage extends PureComponent {
     if (!this.props.user) return (
       <Redirect to="/login" />
     )
-    const data = [["Category", "Amount"], ["Insurance", 700], ["Mobile", 90], ["Health", 100], ["Internet, tv and Phone", 200], ['Other', 25]]
+    let data = [["Category", "Amount"],[]]
     const colors = ['#127ECF', '#90C227', '#F57E18', '#E94435']
     const {firstName, lastName} = this.props.user
+    if(this.props.transactions) {
+    data = calculateTransactions(this.props.transactions)
+    }
+    console.log(data)
     return(
       <Grid container alignContent={'center'} alignItems={'center'} style={{width: '100%'}} spacing={40}>
         <Grid xs={12} item>
           <Typography>
             Hi {firstName} {lastName}! Here is an overview of your transactions.
               {
-                this.props ? console.log(this.props.user) : console.log('nope')
+                this.props.user !== null && this.props.user ? console.log(this.props.user) : console.log('nope')
               }
-            {pieGraph({data, colors})}
+            {
+              pieGraph({data, colors})
+            }
           </Typography>
         </Grid>
       </Grid>
