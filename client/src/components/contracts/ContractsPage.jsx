533fd22ae6a3584a7c71afc9e2180defcdfe4a7d
Charise Walraven <cherri.walraven@gmail.com>
2018-05-16 15:58:46 +0200

    add types/categories to contracts page

--- client/src/components/contracts/ContractsPage.jsx
+++ client/src/components/contracts/ContractsPage.jsx
@@ -3,12 +3,12 @@ import Typography from 'material-ui/Typography'
 import { connect } from 'react-redux'
 import { fetchTransactions } from '../../actions/transactions'
 
-const renderContract = ({...props}) => {
+const renderContract = ({ ...props }) => {
   console.log(props)
-  return(
+  return (
     <Typography>
       <Typography variant='headline'>{props.contractName}</Typography>
-      <p>{props.value}</p>
+      <Typography>{props.value}</Typography>
     </Typography>
   )
 }
@@ -19,20 +19,42 @@ class ContractsPage extends PureComponent {
       this.props.fetchTransactions(1)
     }
   }
-  render(){
+  render() {
     // console.log(this.props)
-    const {transactions} = this.props
+    const { transactions } = this.props
     console.log(transactions)
-    return(
+    return (
       <div>
         {
-          transactions? console.log(transactions) : console.log('nope')
+          transactions ? console.log(transactions) : console.log('nope')
         }
+        <Typography style={{margin: '0 0 30px 0'}} variant='display1'> Insurance
         {
-          transactions ? transactions.map(t => {
-            return renderContract(t)
-          }) : <p>Contracts loading...</p>
-        }
+            transactions ? transactions.map(t => {
+              console.log(t.type)
+              return t.type === 'insurance' ?
+                (renderContract(t)) : null
+            }) : <p>Contracts loading...</p>
+          }
+        </Typography>
+        <Typography style={{ margin: '30px 0' }} variant='display1'> Telecom
+          {
+            transactions ? transactions.map(t => {
+              console.log(t.type)
+              return t.type === 'telecom' ?
+                (renderContract(t)) : null
+            }) : <p>Contracts loading...</p>
+          }
+        </Typography>
+        <Typography style={{ margin: '30px 0' }} variant='display1'> Energy
+        {
+            transactions ? transactions.map(t => {
+              console.log(t.type)
+              return t.type === 'energy' ?
+                (renderContract(t)) : null
+            }) : <p>Contracts loading...</p>
+          }
+        </Typography>
       </div>
     )
   }
