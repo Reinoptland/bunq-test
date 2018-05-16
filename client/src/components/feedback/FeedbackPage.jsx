e88a06fae79105131513472084eb39537c1ac60c
Preeti <singhp1992@gmail.com>
2018-05-16 14:03:55 +0200

    rejected privacy feedback complete - need to add styling and rerouting

--- client/src/components/feedback/FeedbackPage.jsx
+++ client/src/components/feedback/FeedbackPage.jsx
@@ -16,22 +16,23 @@ class FeedbackPage extends PureComponent {
             <form> 
 
                 <h1>Feedback</h1>
-                <div> Why did you not accept?
-                    <br/>
-                    <input type="radio" id="radioButton"/>
-                    <label label> I don't have Bunq </label> 
+                <div className="feedbackForm"> 
+                    <h2>Why did you not accept?</h2>
+                    <input type="radio" id="contactChoice1" name="contact" value="option1"/>
+                    <label for="contactChoice1"> I don't have Bunq </label> 
                     <br />
-                    <input type="radio" id="radioButton" />
-                    <label label> I don't understand PSD </label>
+                    <input type="radio" id="contactChoice2" name="contact" value="option2" />
+                    <label for="contactChoice2"> I don't understand PSD </label>
                     <br />
-                    <input type="radio" id="radioButton" />
-                    <label label> I don't trust the internet </label>
+                    <input type="radio" id="contactChoice3" name="contact" value="option3"  />
+                    <label for="contactChoice3"> I don't trust the internet </label>
                     <br />
-                    <input type="radio" id="radioButton" />
-                    <label label> I changed my mind </label>   
+                    <input type="radio" id="contactChoice4" name="contact" value="option4" />
+                    <label for="contactChoice4"> I changed my mind </label>  
+                    <br /> 
                     <br/> 
-                    <label label> Additional Remarks</label>
-                    <input type="text" id="text"/>
+                    <label> Additional Remarks:</label>
+                    <input type="text" id="text" />
                     <br/>
                     <button type="submit">Submit</button>
                 </div>
