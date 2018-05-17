4f7c5c43ea6b5143e27ef926cf77e5c4e90fde5e
Preeti <singhp1992@gmail.com>
2018-05-17 17:25:51 +0200

    spacing

--- client/src/components/layout/bottomNav.jsx
+++ client/src/components/layout/bottomNav.jsx
@@ -6,17 +6,13 @@ class BottomNav extends Component {
     render() {
         return (
             <div className='bottom-nav'>
-                <div className='left-icons'>
+                <div className='bottom-nav'>
                     <a href='/dashboard'>
                     <img src="../../../icons/home.svg" alt="home-icon"></img>
                     </a>
                     <a href='/contracts'>
                     <img src="../../../icons/contracten.svg" alt="contract-icon"></img>
                     </a>
-                </div>
-                <div className='add-button'>
-                </div>
-                <div className='right-icons' >
                     <a href='/dashboard'>
                     <img src="../../../icons/advies.svg" alt="advies-icon"></img>
                     </a>
