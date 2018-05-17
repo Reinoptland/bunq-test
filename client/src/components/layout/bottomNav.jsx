01dda55f4228b750c22b00bb3dffc551f3e3a583
Preeti <singhp1992@gmail.com>
2018-05-17 15:53:30 +0200

    able to redirect to page, need to add toggle menu for meer and correct redirection for advies

--- client/src/components/layout/bottomNav.jsx
+++ client/src/components/layout/bottomNav.jsx
@@ -7,15 +7,22 @@ class BottomNav extends Component {
         return (
             <div className='bottom-nav'>
                 <div className='left-icons'>
+                    <a href='/dashboard'>
                     <img src="../../../icons/home.svg" alt="home-icon"></img>
+                    </a>
+                    <a href='/contracts'>
                     <img src="../../../icons/contracten.svg" alt="contract-icon"></img>
+                    </a>
                 </div>
                 <div className='add-button'>
-                    
                 </div>
-                <div className='right-icons'>
+                <div className='right-icons' >
+                    <a href='/dashboard'>
                     <img src="../../../icons/advies.svg" alt="advies-icon"></img>
+                    </a>
+                    <a href='/dashboard'>
                     <img src="../../../icons/meer.svg" alt="meer-icon"></img>
+                    </a>
                 </div>
 
             </div>
