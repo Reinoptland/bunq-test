d068efeefecfec8dccf4110eb79eed64caa8e66f
LisaScorzon <lisabailey.eu@gmail.com>
2018-05-22 12:46:50 +0200

    fixed state, deleted reducers

--- client/src/reducers/currentUser.js
+++ client/src/reducers/currentUser.js
@@ -1,5 +1,11 @@
 import { USER_LOGIN_SUCCESS, USER_LOGOUT, UPDATE_USER, DELETE_USER } from '../actions/users'
 import { localStorageJwtKey } from '../constants'
+import { ADD_USER, FETCH_USER_PROFILE  } from '../actions/users'
+
+
+
+
+
 
 let initialState = null
 
@@ -16,21 +22,77 @@ catch (e) {
 
 export default function (state = initialState, { type, payload }) {
   switch (type) {
+    
     case USER_LOGIN_SUCCESS:
       return payload
 
-    case USER_LOGOUT:
+    
+      case USER_LOGOUT:
       return null
 
-    case "UPDATE_USER":
+    
+      case "UPDATE_USER":
       // state.user = payload
       return {...state, user: payload}
     
-    case DELETE_USER:
+    
+      case DELETE_USER:
       return delete payload.id
       console.log("delete user case in reducer", payload.id)
+
+     
+      case UPDATE_USER:
+      console.log("reducer update user", type, payload)
+      return {
+        ...state,
+         payload
+      } 
+
+      case ADD_USER:
+      return {
+        ...state,
+        [payload.id]: payload
+      }
+      
+
+      case FETCH_USER_PROFILE:
+console.log("reducer FETCH PROFILE", type, payload)
+
+      return payload
+
 
     default:
       return state
   }
 }
+// export default (state = null, { type, payload }) => {
+//   switch (type) {
+//     case ADD_USER:
+//       return {
+//         ...state,
+//         [payload.id]: payload
+//       }
+
+//     default:
+//       return state
+//   }
+// }
+
+// export default (state = null, { type, payload }) => {
+//   switch (type) {
+// case UPDATE_USER:
+// console.log("reducer update user", type, payload)
+//       return {
+//         ...state,
+//          payload
+//       } 
+
+// case FETCH_USER_PROFILE:
+// console.log("reducer FETCH PROFILE", type, payload)
+
+//       return payload
+      
+//       default:
+//       return state
+//   } 
+// }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,3 @@
-import users from './users'
 import signup from './signup'
 import currentUser from './currentUser'
 import login from './login'
@@ -6,11 +5,10 @@ import bunq from './bunq'
 import privacy from './privacy'
 import feedback from './feedback'
 import transactions from './transactions'
-import user from './user'
+
 
 
 export default {
-    users,
     signup,
     login,
     bunq,
@@ -18,5 +16,5 @@ export default {
     privacy,
     feedback,
     transactions,
-    user
+    
 }
