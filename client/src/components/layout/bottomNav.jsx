5d5d09018850c72e085e9d534a4a3bb39e5b8e6e
abbasalwash <abbas@alwash.nl>
2018-05-23 15:50:53 +0200

    fixed icons & double contracts

--- client/src/components/layout/bottomNav.jsx
+++ client/src/components/layout/bottomNav.jsx
@@ -13,19 +13,19 @@ const BottomNav = (props) => {
     return (
         <div className='bottom-nav'>
             <div className='left-icons'>
-                {location.pathname.indexOf('dashboard') < 0 && <Link to={'/dashboard'}><img src="icons/home.svg" alt="home-icon"></img></Link>}
-                {location.pathname.indexOf('dashboard') > 0 && <Link to={'/dashboard'}><img src="icons/homeActive.svg" alt="home-icon"></img></Link>}
-                {location.pathname.indexOf('contracts') < 0 && <Link to={'/contracts'}><img src="icons/contracten.svg" alt="contracts-icon"></img></Link>}
-                {location.pathname.indexOf('contracts') > 0 && <Link to={'/contracts'}><img src="icons/contractenActive.svg" alt="contracts-icon"></img></Link>}
+                {location.pathname.indexOf('dashboard') < 0 && <Link to={'/dashboard'}><img src="/icons/home.svg" alt="home-icon"></img></Link>}
+                {location.pathname.indexOf('dashboard') > 0 && <Link to={'/dashboard'}><img src="/icons/homeActive.svg" alt="home-icon"></img></Link>}
+                {location.pathname.indexOf('contracts') < 0 && <Link to={'/contracts'}><img src="/icons/contracten.svg" alt="contracts-icon"></img></Link>}
+                {location.pathname.indexOf('contracts') > 0 && <Link to={'/contracts'}><img src="/icons/contractenActive.svg" alt="contracts-icon"></img></Link>}
             </div>
             <div className='add-button'>
-                {location.pathname.indexOf('csv') < 0 && <Link to={'/csv'}><img src="icons/addButton.svg" alt="add-icon"></img></Link>}
-                {location.pathname.indexOf('csv') > 0 && <Link to={'/csv'}><img src="icons/addButtonActive.svg" alt="add-icon"></img></Link>}
+                {location.pathname.indexOf('csv') < 0 && <Link to={'/csv'}><img src="/icons/addButton.svg" alt="add-icon"></img></Link>}
+                {location.pathname.indexOf('csv') > 0 && <Link to={'/csv'}><img src="/icons/addButtonActive.svg" alt="add-icon"></img></Link>}
             </div>
             <div className='right-icons'>
-                {location.pathname.indexOf('advies') > 0 && <Link to={'/advies'}><img src="icons/adviesActive.svg" alt="advice-icon"></img></Link>}
-                {location.pathname.indexOf('advies') < 0 && <Link to={'/advies'}><img src="icons/advies.svg" alt="advice-icon"></img></Link>}
-                <Link to={'/logout'}><img src="icons/loguit.svg" alt="loguit-icon"></img></Link>
+                {location.pathname.indexOf('advies') > 0 && <Link to={'/advies'}><img src="/icons/adviesActive.svg" alt="advice-icon"></img></Link>}
+                {location.pathname.indexOf('advies') < 0 && <Link to={'/advies'}><img src="/icons/advies.svg" alt="advice-icon"></img></Link>}
+                <Link to={'/logout'}><img src="/icons/loguit.svg" alt="loguit-icon"></img></Link>
             </div>
 
         </div>
