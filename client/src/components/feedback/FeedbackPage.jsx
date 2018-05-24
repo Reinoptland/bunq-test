0a1b59d9bc8403014e5c5cef0e6822a265578aa7
LisaScorzon <lisabailey.eu@gmail.com>
2018-05-24 09:11:23 +0200

    adding styling to feedback

--- client/src/components/feedback/FeedbackPage.jsx
+++ client/src/components/feedback/FeedbackPage.jsx
@@ -2,25 +2,30 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { feedback } from '../../actions/users'
 import FeedbackForm from './FeedbackForm'
+import { Paper } from '@material-ui/core';
 
 class FeedbackPage extends PureComponent {
   handleSubmit = (data) => {
-    // console.log(data)
+  
     this.props.feedback(data, this.props.user.id)
-    // alert.show('Thanks for your feedback!');
+    
     
     setTimeout(_ => {window.location = '/logout'}, 3100);  
 }
 
     render() {
         return (
+            <Paper>
             <div>
 
                 <h1 style={{textAlign: 'center', margin:'0 0 0 -5px'}}>Feedback</h1>
                 <div className="feedbackForm">
                     <FeedbackForm onSubmit={this.handleSubmit} />
+                    <br/>
+                    <br/>
                 </div>
             </div>
+            </Paper>
         )
     }
 }
